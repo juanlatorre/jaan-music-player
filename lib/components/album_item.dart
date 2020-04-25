02e818845d8eeed79a468b5f875ad000a72f9c15
Juan Latorre <juanlatorre@protonmail.com>
2020-04-25 12:51:02 -0400

    .

--- lib/components/album_item.dart
+++ lib/components/album_item.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 
 Widget albumItem(BuildContext context, String src) {
   return Padding(
-    padding: EdgeInsets.all(8.0),
-    child: SizedBox(
-      height: MediaQuery.of(context).size.width / 2.5,
-      width: MediaQuery.of(context).size.width / 2.5,
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(
-          Radius.circular(5),
-        ),
-        child: Image.network(
-          src,
-          width: double.infinity,
-          fit: BoxFit.fill,
+    padding: EdgeInsets.all(8),
+    child: Material(
+      elevation: 4.0,
+      color: Colors.transparent,
+      child: Ink.image(
+        image: NetworkImage(src),
+        fit: BoxFit.cover,
+        width: MediaQuery.of(context).size.width / 2.5,
+        height: MediaQuery.of(context).size.width / 2.5,
+        child: InkWell(
+          onTap: () {
+            print("yay");
+          },
+          child: null,
         ),
       ),
     ),
--- lib/components/boton_biblioteca.dart
+++ lib/components/boton_biblioteca.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:jaan_music_player/utils/indian.dart';
 
 Widget botonBiblioteca(String titulo) {
-  return InkWell(
-    child: SizedBox(
-      width: 100,
-      child: Container(
-        padding: EdgeInsets.only(top: 5, bottom: 5),
-        child: Text(
-          titulo,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            color: indian["bluebell"],
-            fontSize: 18,
+  return Material(
+    color: Colors.white,
+    child: InkWell(
+      child: SizedBox(
+        width: 100,
+        child: Container(
+          padding: EdgeInsets.only(top: 5, bottom: 5),
+          child: Text(
+            titulo,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: indian["bluebell"],
+              fontSize: 18,
+            ),
           ),
         ),
       ),
+      onTap: () {
+        print("jaja");
+      },
     ),
-    onTap: () {
-      print("jaja");
-    },
   );
 }
